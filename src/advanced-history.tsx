5423e289f6eb007cd755ef77980c54759569b57a
ephys <hello@guylian.me>
2021-06-26 12:23:11 +0200

    feat: history - add goBackToMatch & goBackToKey

--- src/advanced-history.tsx
+++ src/advanced-history.tsx
@@ -1,10 +1,11 @@
-import type { History as RouterHistory, Location, LocationDescriptor } from 'history';
+import type { History as RouterHistory, Location, LocationDescriptor, Path } from 'history';
 import { useMemo } from 'react';
 import { useHistory } from 'react-router-dom';
+import { MaybePromise } from '../utils/types';
 import { useLocationLocker, LockLocation } from './location-locker';
 
-export type AdvancedHistory<State = unknown> = {
-  history: RouterHistory<State>,
+export interface AdvancedHistory<State = unknown> {
+  history: RouterHistory<State>;
 
   /**
    * Navigate will block location updates until the promise returned by {cb} resolves
@@ -12,22 +13,29 @@ export type AdvancedHistory<State = unknown> = {
    *
    * NB. Each navigation action has a delay, use sparingly.
    */
-  navigate: (cb: () => Promise<void> | void) => Promise<void>,
+  navigate<T>(cb: () => Promise<T> | T): Promise<T>;
 
-  /** Like {@link RouterHistory.goBack} but returns a promise that resolves once successfully navigated */
-  goBackAsync: () => Promise<void>,
+  /**
+   * Like {@link RouterHistory.goBack} but returns a promise that resolves once successfully navigated
+   *
+   * @param {boolean} preventLeavingApp - Cancel operation if there is no previous entry in the history specific to this SPA instance.
+   *
+   * @returns A promise that resolves to true if the application went back, or false if {preventLeavingApp} is true and there is no previous entry.
+   */
+  goBackAsync(preventLeavingApp?: boolean): Promise<boolean>;
 
   /** Like {@link RouterHistory.goForward} but returns a promise that resolves once successfully navigated */
-  goForwardAsync: () => Promise<void>,
+  goForwardAsync(): Promise<boolean>;
 
   /** Like {@link RouterHistory.go} but returns a promise that resolves once successfully navigated */
-  goAsync: (n: number) => Promise<void>,
+  goAsync(n: number): Promise<void>;
 
   /** Like {@link RouterHistory.push} but returns a promise that resolves once successfully navigated */
-  pushAsync: (path: string | LocationDescriptor, state?: any) => Promise<void>,
+  pushAsync(location: LocationDescriptor<State>): Promise<void>;
+  pushAsync(path: Path, state?: State): Promise<void>;
 
   /** Like {@link RouterHistory.replace} but returns a promise that resolves once successfully navigated */
-  replaceAsync: (path: string | LocationDescriptor, state?: any) => Promise<void>,
+  replaceAsync(path: LocationDescriptor<State>, state?: any): Promise<void>;
 
   /**
    * This method will roll back the history until the desired destination is reached.
@@ -37,12 +45,38 @@ export type AdvancedHistory<State = unknown> = {
    *
    * @param location
    * @param fallback
+   *
+   * @deprecated use {@see AdvancedHistory#goBackToKey}
    */
-  goBackUntil: (
+  goBackUntil(
     location: (Partial<Location<State>> | TLocationCb<State>),
     fallback?: Partial<Location<State>>
-  ) => Promise<void>,
-};
+  ): Promise<void>;
+
+  /**
+   * Goes backwards in the history to the history entry for which {locationMatcher} returns true.
+   *
+   * If {locationMatcher} does not return true for any history entry and {fallback} is defined, it pushes {fallback} as a new route.
+   * If not defined, the history position remains unchanged.
+   *
+   * @param {TLocationCb<State>} locationMatcher
+   * @param {Location<State>} fallback - The new location that will be pushed if {key} is not found.
+   * @returns {Promise<boolean>} A promise that resolves once navigation is complete. It resolves to true if the app navigated, and false otherwise.
+   */
+  goBackToMatch(locationMatcher: TLocationCb<State>, fallback?: LocationDescriptor<State>): Promise<boolean>;
+
+  /**
+   * Goes backwards in the history to the entry matching {key} (if it exists in the history).
+   *
+   * If the entry does not exist in history and {fallback} is defined, it pushes {fallback} as a new route.
+   * If not defined, the history position remains unchanged.
+   *
+   * @param {string} key - The ID of the history entry to return to.
+   * @param {Location<State>} fallback - The new location that will be pushed if {key} is not found.
+   * @returns {Promise<boolean>} A promise that resolves to true if the app navigated to the key, and false otherwise.
+   */
+  goBackToKey(key: string, fallback?: LocationDescriptor<State>): Promise<boolean>;
+}
 
 type TLocationCb<State> = (loc: Location<State>) => boolean;
 
@@ -54,16 +88,22 @@ export function useAdvancedHistory<State>(): AdvancedHistory<State> {
     const advHistory: AdvancedHistory<State> = {
       history,
       navigate: callback => navigate(callback, lock),
-      goBackAsync: () => goBackAsync(history),
-      goForwardAsync: promisify(history, 'goForward'),
+      goBackAsync: (preventLeavingApp?: boolean) => goBackAsync(history, preventLeavingApp),
+      goForwardAsync: () => goForwardAsync(history),
       goAsync: promisify(history, 'go'),
-      pushAsync: promisify(history, 'push'),
+      pushAsync: (path: Path | LocationDescriptor<State>, state?: State) => pushAsync(history, path, state),
       replaceAsync: promisify(history, 'replace'),
       goBackUntil: (
         location: Partial<Location<State>> | TLocationCb<State>,
         fallback?: Partial<Location<State>>,
       ) => {
         return goBackUntil(history, lock, location, fallback);
+      },
+      goBackToMatch: (locationMatcher: TLocationCb<State>, fallback?: LocationDescriptor<State>) => {
+        return goBackToMatch(history, lock, locationMatcher, fallback);
+      },
+      goBackToKey: (key: string, fallback?: LocationDescriptor<State>) => {
+        return goBackToKey(history, lock, key, fallback);
       },
     };
 
@@ -79,8 +119,31 @@ function promisify(history: RouterHistory, methodName: string) {
   return method;
 }
 
-function goBackAsync(history: RouterHistory) {
-  return waitForHistory(history, () => history.goBack());
+function pushAsync<State>(
+  history: RouterHistory<State>,
+  path: LocationDescriptor<State>,
+  state?: State,
+): Promise<void> {
+  // @ts-expect-error
+  return waitForHistory(history, () => history.push(path, state));
+}
+
+function goForwardAsync<State>(history: RouterHistory<State>): Promise<boolean> {
+  // we don't currently have a way to determine if we're on the history entry of the SPA
+  // so for now we can't cancel it
+  return waitForHistory(history, () => history.goForward()).then(() => true);
+}
+
+function goBackAsync<State>(history: RouterHistory<State>, preventLeavingApp: boolean = false): Promise<boolean> {
+  if (preventLeavingApp) {
+    const previousLocation = history.location.state?.previousLoc;
+
+    if (!previousLocation) {
+      return Promise.resolve(false);
+    }
+  }
+
+  return waitForHistory(history, () => history.goBack()).then(() => true);
 }
 
 function waitForHistory(history: RouterHistory, callback: () => void) {
@@ -94,10 +157,89 @@ function waitForHistory(history: RouterHistory, callback: () => void) {
   });
 }
 
-async function navigate(callback, lock) {
+async function navigate<T>(callback: () => MaybePromise<T>, lock): Promise<T> {
   const unlock = lock();
-  await callback();
+  const returnVal = await callback();
   unlock();
+
+  return returnVal;
+}
+
+function goBackToMatch<State>(history: RouterHistory<State>, lock: LockLocation,
+  matcher: TLocationCb<State>, fallback?: LocationDescriptor<State>): Promise<boolean> {
+  return navigate(async () => {
+    const startingKey = history.location.key;
+
+    const success = await goBackToMatchImpl<State>(history, matcher);
+    if (success) {
+      return true;
+    }
+
+    await goForwardToKeyImpl(history, startingKey);
+
+    if (fallback) {
+      await pushAsync(history, fallback);
+    }
+
+    return false;
+  }, lock);
+}
+
+async function goBackToMatchImpl<State>(history: RouterHistory<State>, matcher: TLocationCb<State>): Promise<boolean> {
+  while (!matcher(history.location)) {
+    // eslint-disable-next-line no-await-in-loop
+    const success = await goBackAsync(history, true);
+    if (!success) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function goBackToKey<State>(history: RouterHistory<State>, lock: LockLocation,
+  key: string, fallback?: LocationDescriptor<State>): Promise<boolean> {
+
+  return navigate(async () => {
+    const startingKey = history.location.key;
+
+    const success = await goBackToKeyImpl<State>(history, key);
+    if (success) {
+      return true;
+    }
+
+    await goForwardToKeyImpl(history, startingKey);
+
+    if (fallback) {
+      await pushAsync(history, fallback);
+    }
+
+    return false;
+  }, lock);
+}
+
+async function goBackToKeyImpl<State>(history: RouterHistory<State>, key: string): Promise<boolean> {
+  while (history.location.key !== key) {
+    // eslint-disable-next-line no-await-in-loop
+    const success = await goBackAsync(history, true);
+    if (!success) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+async function goForwardToKeyImpl<State>(history: RouterHistory<State>, key: string): Promise<boolean> {
+  while (history.location.key !== key) {
+    // eslint-disable-next-line no-await-in-loop
+    const success = await goForwardAsync(history);
+    if (!success) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
 /**
@@ -133,9 +275,8 @@ async function goBackUntilImpl<State>(
     return;
   }
 
-  const previousLocation = history.location.state?.previousLoc;
-
-  if (!previousLocation) {
+  const success = await goBackAsync(history);
+  if (!success) {
     if (!fallback) {
       if (typeof location !== 'function') {
         fallback = location;
@@ -149,7 +290,6 @@ async function goBackUntilImpl<State>(
     return;
   }
 
-  await goBackAsync(history);
   await goBackUntilImpl(history, location, fallback);
 }
 
